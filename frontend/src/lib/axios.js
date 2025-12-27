b1e845e67bfc6c1e188605fd58352f356d119914
Thanga Preethi <thangapreethi85@gmail.com>
2025-12-27 21:05:17 +0530

    Fix 404 error while login

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "http://localhost:5000/api"
-    : "https://mernstack-chat-app-production.up.railway.app/api",
+  baseURL: "https://mernstack-chat-app-production.up.railway.app/api",
   withCredentials: true,
 });
